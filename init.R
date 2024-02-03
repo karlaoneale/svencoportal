3d568fa79c89037fd97335210a7a8764b5fae8cf
Karla O'Neale <karlaoneale@gmail.com>
2024-02-03 19:41:22 +0200

    Run files

---
+++ init.R
@@ -0,0 +1,13 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+my_packages = c("httr", "shinyjs", "DT", "ggplot2", "tidyr", "dplyr", "googledrive", "shiny", "googlesheets4", 
+                "shinydashboard", "RCurl", "googledrive", "jpeg", "RSQLite", "shinyWidgets", "xml2",
+                "colourpicker", "daterangepicker", "timevis", "lubridate", "jsonlite", "RPostgres", "DBI")
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
