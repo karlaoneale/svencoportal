eb976c7155daddfadd23126df960c0a0802da039
Karla O'Neale <karlaoneale@gmail.com>
2024-06-09 20:49:06 +0200

    shs

--- init.R
+++ init.R
@@ -2,15 +2,16 @@
 #
 # Example R code to install packages if not already installed
 #
+
+install_if_missing("githubinstall")
+library(githubinstall)
+githubinstall("shinyStore")
 my_packages = c("httr", "shinyjs", "DT", "ggplot2", "tidyr", "dplyr", "googledrive", "shiny", "googlesheets4", 
                 "shinydashboard", "RCurl", "googledrive", "jpeg", "RSQLite", "shinyWidgets", "xml2",
-                "colourpicker", "daterangepicker", "timevis", "lubridate", "jsonlite", "RPostgres", "DBI", "githubinstall")
+                "colourpicker", "daterangepicker", "timevis", "lubridate", "jsonlite", "RPostgres", "DBI")
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
   }
 }
 invisible(sapply(my_packages, install_if_missing))
-
-library(githubinstall)
-githubinstall("shinyStore")
