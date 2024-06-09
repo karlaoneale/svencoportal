cb67087658f31f79f2b76598077934679bb7a2de
Karla O'Neale <karlaoneale@gmail.com>
2024-06-09 21:08:28 +0200

    shs

--- init.R
+++ init.R
@@ -2,15 +2,24 @@
 #
 # Example R code to install packages if not already installed
 #
-my_packages = c("githubinstall", "httr", "shinyjs", "DT", "ggplot2", "tidyr", "dplyr", "googledrive", "shiny", "googlesheets4", 
-                "shinydashboard", "RCurl", "googledrive", "jpeg", "RSQLite", "shinyWidgets", "xml2",
-                "colourpicker", "daterangepicker", "timevis", "lubridate", "jsonlite", "RPostgres", "DBI")
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p)
+my_packages <- c("httr", "shinyjs", "DT", "ggplot2", "tidyr", "dplyr", "googledrive", "shiny", "googlesheets4", 
+                 "shinydashboard", "RCurl", "googledrive", "jpeg", "RSQLite", "shinyWidgets", "xml2", "colourpicker", 
+                 "daterangepicker", "timevis", "lubridate", "jsonlite", "RPostgres", "DBI")
+
+install_if_missing <- function(p) {
+  if (!requireNamespace(p, quietly = TRUE)) {
+    install.packages(p, dependencies = TRUE)
   }
 }
+
 invisible(sapply(my_packages, install_if_missing))
-# 
-# library(githubinstall)
-# githubinstall("shinyStore")
+
+# Ensure 'remotes' package is installed for installing from GitHub
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+
+# Install shinyStore from GitHub if not already installed
+if (!requireNamespace("shinyStore", quietly = TRUE)) {
+  remotes::install_github("trestletech/shinyStore")
+}
